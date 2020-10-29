52ee13bdec072f50d7bb9a01b7725b9e79a4072b
Débora <deboracristinapereira123@gmail.com>
2020-10-29 15:47:50 -0300

    start api

---
+++ crudHotelBackend/src/main/java/com/senior/crudHotelBackend/CrudHotelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.senior.crudHotelBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudHotelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudHotelBackendApplication.class, args);
+	}
+
+}
---
+++ crudHotelBackend/src/test/java/com/senior/crudHotelBackend/CrudHotelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senior.crudHotelBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudHotelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

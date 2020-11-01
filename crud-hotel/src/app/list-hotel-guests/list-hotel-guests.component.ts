7cdcdc5ccdb66a18f03dd4ebf4f052428ab84f35
Débora <deboracristinapereira123@gmail.com>
2020-11-01 04:33:07 -0300

    Valores hospedes

--- crud-hotel/src/app/list-hotel-guests/list-hotel-guests.component.ts
+++ crud-hotel/src/app/list-hotel-guests/list-hotel-guests.component.ts
@@ -1,3 +1,4 @@
+import { invalid } from '@angular/compiler/src/render3/view/util';
 import { Component, OnInit } from '@angular/core';
 import { HotelService } from '../hotel.service';
 import { Checkin } from '../models/checkin/checkin';
@@ -15,8 +16,13 @@ export class ListHotelGuestsComponent implements OnInit {
   
   dataList: Array<{ name: string, document: string, value: number }> = [];
 
-  weekDayValue: number = 120;
-  weekendDayValue: number = 150;
+  totalDays: Array<number> = [];
+  countDayWeekends: Array<number> = [];
+
+  values: Array<number> = [];
+
+  weekDayValue: number = 120.00;
+  weekendDayValue: number = 150.00;
 
   constructor(private hotelService: HotelService) { }
 
@@ -24,34 +30,79 @@ export class ListHotelGuestsComponent implements OnInit {
     this.getCheckin();
   }
 
-  private getCheckin() {
+  getCheckin() {
     this.hotelService.getCheckinList().subscribe(data => {
       this.checkIn = data;
-      console.log(data)
 
-      for (var i = 0; i < data.length; i++) {
-
-        // Calcular valor
-
-        const milisecondsDateIn = new Date(data[i].dataEntrada).getMilliseconds()
-        const milisecondsDateOut = new Date(data[i].dataSaida).getMilliseconds()
-
-        console.log("Mili in " + milisecondsDateIn)
-        console.log("Mili out " + milisecondsDateOut)
-
-        const milisecondsOneDay = 1000 * 60 * 60 * 24;
-
-        var days = (milisecondsDateIn - milisecondsDateOut) / milisecondsOneDay
-
-        console.log("Dias " + days)
-      }
+      this.calcDays(data);
 
       // Preencher tabela 
 
       for (var i = 0; i < data.length; i++) {
-        this.dataList.push({name: data[i].hospede.nome, document: data[i].hospede.documento, value: 0})
+        this.dataList.push({name: data[i].hospede.nome, document: data[i].hospede.documento, value: this.values[i]})
       }
     })
   }
 
+  calcDays(data: Array<Checkin>) {
+
+    for (var i = 0; i < data.length; i++) {
+
+      // Diferenca de dias
+
+      const dateInMs = new Date(data[i].dataEntrada).getTime()
+      const dateOutMs = new Date(data[i].dataSaida).getTime()
+
+      const msOneDay = 1000 * 60 * 60 * 24
+      this.totalDays.push(Math.round((dateOutMs - dateInMs)/msOneDay))
+
+      // Final de semana
+
+      var count = 0
+      var dayWeek = 0
+
+      var dateInStart = new Date(data[i].dataEntrada);
+
+      dayWeek = dateInStart.getDay()
+
+      if (dayWeek == 0 || dayWeek == 6) {
+        count++;
+      }
+
+      while (dateInStart < new Date(data[i].dataSaida)) {
+        dateInStart.setDate(dateInStart.getDate() + 1)
+
+        dayWeek = dateInStart.getDay()
+
+        if (dayWeek == 0 || dayWeek == 6) {
+          count++;
+        }
+
+      }
+
+      this.countDayWeekends.push(count)
+    } 
+
+    this.calcValues();
+
+  }
+
+  calcValues() {
+
+    var valueDayWeek = 0.00
+    var valueDayWeekend = 0.00
+    var valueTotalDays = 0.00
+
+    for (var i = 0; i < this.countDayWeekends.length; i++) {
+      valueDayWeek = (this.totalDays[i] - this.countDayWeekends[i]) * this.weekDayValue
+      valueDayWeekend = this.countDayWeekends[i] * this.weekendDayValue
+      valueTotalDays = valueDayWeek + valueDayWeekend
+
+      this.values.push(valueTotalDays)
+    }
+
+  }
+
+    
 }
+

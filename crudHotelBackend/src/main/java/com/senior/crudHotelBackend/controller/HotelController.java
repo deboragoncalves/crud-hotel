0b0cb0e86a5668a4757790d788fe0cf3b7953241
Débora <deboracristinapereira123@gmail.com>
2020-11-02 01:28:42 -0300

    Put e patch - api

--- crudHotelBackend/src/main/java/com/senior/crudHotelBackend/controller/HotelController.java
+++ crudHotelBackend/src/main/java/com/senior/crudHotelBackend/controller/HotelController.java
@@ -3,12 +3,14 @@ package com.senior.crudHotelBackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,14 +49,51 @@ public class HotelController {
 
 	// Post
 	
-	@PostMapping("/guests")
+	@PostMapping("/hospedes/post")
 	public Hospede createGuest(@RequestBody Hospede hospede) {
 		return hospedeRepository.save(hospede);
 	}
 	
-	@PostMapping(value="/createCheckin")
+	@PostMapping(value="/checkin/post")
 	public Checkin createCheckin(@RequestBody Checkin checkin) {
 		return checkinRepository.save(checkin);
 	}
 	
+	// Patch
+	
+	@RequestMapping(value="/checkin/patch/{id}", method=RequestMethod.PATCH)
+	public ResponseEntity<Checkin> updateCheckinDateOut(@PathVariable Long id, @RequestBody Checkin checkinUpdate) {
+		Checkin checkin = checkinRepository.findById(id).orElse(null);
+		
+		if (checkin == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		checkin.setDataSaida(checkinUpdate.getDataSaida());
+		
+		checkinRepository.save(checkin);
+		
+		return ResponseEntity.ok(checkin);
+				
+	}
+	
+	// Put
+	
+	@RequestMapping(value="/checkin/put/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Checkin> updateCheckin(@PathVariable Long id, @RequestBody Checkin checkinUpdate) {
+		Checkin checkin = checkinRepository.findById(id).orElse(null);
+		
+		if (checkin == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		checkin.setAdicionalVeiculo(checkinUpdate.isAdicionalVeiculo());
+		checkin.setDataEntrada(checkinUpdate.getDataEntrada());
+		checkin.setDataSaida(checkinUpdate.getDataSaida());
+		
+		checkinRepository.save(checkin);
+		
+		return ResponseEntity.ok(checkin);
+				
+	}
 }
